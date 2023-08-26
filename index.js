39df49b66c725b26f69c31d06e465ef4bf28b420
Nainglynn <nainglynnxd@gmail.com>
2023-08-27 00:38:56 +0100

    Footer links done for large screens

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-const header = document.getElementById("header");
-function onScroll() {
-  header.classList.add("box-shadow");
-}
+footers = [
+  {
+    header: "Platform",
+    links: [
+      "Pricing",
+      "Sign up free",
+      "Community",
+      "Website builder",
+      "Digital Products",
+      "Email Marketing",
+    ],
+  },
+  {
+    header: "Support",
+    links: [
+      "Contact us",
+      "Help center",
+      "Hire a podia pro",
+      "Creator community",
+      "Podia 101",
+    ],
+  },
+  {
+    header: "Features",
+    links: [
+      "Online courses",
+      "Downloads",
+      "Webinars",
+      "Coaching",
+      "All features",
+    ],
+  },
+  {
+    header: "Social",
+    links: ["Twitter", "Facebook", "Instagram", "Youtube"],
+  },
+];
+
+const footer = document.getElementById("footer");
+
+footers.map(
+  (item) =>
+    (footer.innerHTML += `
+  <div>
+    <button>${item.header}</button>
+    <ul class="lists">
+      ${item.links
+        .map(
+          (link) =>
+            `<li>
+            <a>${link}</a>
+          </li>`
+        )
+        .join(" ")}
+    </ul>
+  </div>
+  `)
+);
+
+footer.innerHTML += `
+      <p class="terms">
+        Podia Labs, Inc. &copy; 2023 <a href="">Terms</a> <a href="">Privacy</a>
+      </p>
+`;
